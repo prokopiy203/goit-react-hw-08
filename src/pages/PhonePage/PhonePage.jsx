f7af8ef5f7a11ca235eaf0146c1fe810d4d379fc
Vlad Prokopiy <vladprokopiy@gmail.com>
2025-06-22 22:34:13 +0300

    fix-fetch-contacts

--- src/pages/PhonePage/PhonePage.jsx
+++ src/pages/PhonePage/PhonePage.jsx
@@ -1,9 +1,18 @@
+import { useDispatch } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import css from "./PhonePage.module.css";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contacts/operations";
 
 export default function PhoneBookPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={css.wrapper}>
       <title>Phone book</title>
